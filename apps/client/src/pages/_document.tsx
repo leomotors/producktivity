093038e40a90b4eba74a1c10d356f3a2ea81d893
Idhibhat Pankam <marginpankam@gmail.com>
2022-10-21 22:15:01 +0700

    commit things from main

--- apps/client/src/pages/_document.tsx
+++ apps/client/src/pages/_document.tsx
@@ -2,25 +2,25 @@ import { Head, Html, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Inter:r,b&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans+Thai:r,b&display=swap"
-          rel="stylesheet"
-        />
-        <Script id="check-dark-light" strategy="beforeInteractive">
-          {`(()=>{const e=localStorage.getItem("producktivity-theme"),a=window.matchMedia("(prefers-color-scheme: dark)").matches;(!e||e==="auto"?a:e==="dark")&&document.documentElement.classList.add("dark")})()`}
-        </Script>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css?family=Inter:r,b&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans+Thai:r,b&display=swap"
+					rel="stylesheet"
+				/>
+				<Script id="check-dark-light" strategy="beforeInteractive">
+					{`(()=>{const e=localStorage.getItem("producktivity-theme"),a=window.matchMedia("(prefers-color-scheme: dark)").matches;(!e||e==="auto"?a:e==="dark")&&document.documentElement.classList.add("dark")})()`}
+				</Script>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
