4903759f08dc88d641cef6038405b2784fd7a5c6
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-10-23 21:48:44 +0700

    chore: remove unused files

--- apps/server/src/@generated/not-test/not-test-min-aggregate.output.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from '@nestjs/graphql';
-import { ObjectType } from '@nestjs/graphql';
-
-@ObjectType()
-export class NotTestMinAggregate {
-
-    @Field(() => String, {nullable:true})
-    id?: string;
-}
