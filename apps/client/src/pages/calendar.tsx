ddb284544da875b0410d57427c504b013069bc49
Idhibhat Pankam <marginpankam@gmail.com>
2022-10-25 00:29:00 +0700

    real time calendar

--- apps/client/src/pages/calendar.tsx
+++ apps/client/src/pages/calendar.tsx
@@ -4,27 +4,54 @@ import DefaultLayout from "../core/general/layouts/default";
 
 const Calendar: NextPageWithLayout = () => {
   const weekDays: string[] = [
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
-    "Sunday",
   ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const now = new Date();
+  const weekDay = new Date(now.getFullYear(), now.getMonth(), 1).getDay();
+  const numOfDays = new Date(
+    now.getFullYear(),
+    now.getMonth() + 1,
+    0
+  ).getDate();
+
   const days = [];
-  for (let i = 1; i <= 30; i++) {
-    days[i - 1] = i;
+  let cou = 1;
+  for (let i = 0; i < weekDay; i++) {
+    days[i] = null;
   }
-  for (let i = 31; i <= 35; i++) {
-    days[i - 1] = null;
+  for (let i = weekDay; i < numOfDays + weekDay; i++) {
+    days[i] = cou;
+    cou += 1;
+  }
+  for (let i = numOfDays + weekDay; i < 42; i++) {
+    days[i] = null;
   }
 
   return (
     <DefaultLayout>
       <div className="h-full flex flex-col ml-8 rounded-lg bg-gray-800 w-11/12 overflow-auto">
         <div className="text-3xl font-bold mb-2 text-white bg-gray-800">
-          October 2022
+          {`${months[now.getMonth()]} ${now.getFullYear()}`}
         </div>
         <div className="z-10 rounded-t-lg grid grid-cols-7 w-full bg-emerald-50">
           {weekDays.map((day, index) => {
