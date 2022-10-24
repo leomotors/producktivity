5678d75150abd1364abfaea181f3b30d283abee5
Matt <kritapassk138@gmail.com>
2022-10-24 18:30:19 +0700

    resolve all comments

--- apps/server/src/task/task.resolver.ts
+++ apps/server/src/task/task.resolver.ts
@@ -6,27 +6,25 @@ import { User } from "@generated/user/user.model";
 import { RequireLogin } from "src/auth/auth.decorator";
 import { UserContext } from "src/user/user.decorator";
 
-import { createTaskArgs, updateTaskArgs } from "./dto/task.dto";
+import { createTaskArgs, updateTaskArgs } from "./task.dto";
 import { TaskService } from "./task.service";
 
-@Resolver()
+@Resolver(() => Task)
+@RequireLogin()
 export class TaskResolver {
   constructor(private readonly service: TaskService) {}
 
   @Mutation(() => Task)
-  @RequireLogin()
   createTask(@Args() input: createTaskArgs, @UserContext() user: User) {
     return this.service.createTask(input, user);
   }
 
   @Mutation(() => Task)
-  @RequireLogin()
   updateTask(@Args() input: updateTaskArgs, @UserContext() user: User) {
     return this.service.updateTask(input, user);
   }
 
   @Mutation(() => Task)
-  @RequireLogin()
   deleteTask(@Args("id") id: string, @UserContext() user: User) {
     return this.service.deleteTask(id, user);
   }
