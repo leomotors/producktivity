06298b19eec62f261cad5e6f6cdbb5a15e0b4d0b
Idhibhat Pankam <marginpankam@gmail.com>
2022-10-24 13:04:31 +0700

    minimize navbar arghhhhhhhhh

--- apps/client/src/core/general/components/Navbar.tsx
+++ apps/client/src/core/general/components/Navbar.tsx
@@ -8,6 +8,8 @@ import ClipboardDocumentCheckIcon from "@heroicons/react/24/outline/ClipboardDoc
 import PlusCircleIcon from "@heroicons/react/24/outline/PlusCircleIcon.js";
 import Squares2X2Icon from "@heroicons/react/24/outline/Squares2X2Icon.js";
 
+import "../../../styles/Navbar.module.scss";
+
 import NavItem from "./NavItem";
 
 const Navbar: FC = () => {
@@ -19,68 +21,49 @@ const Navbar: FC = () => {
     "transition ease-in-out delay-50 duration-150 hover:scale-150";
   return (
     <>
-      {isMinimized ? (
-        <div className="p-8 flex flex-row md:flex-col items-baseline md:items-center space-x-4 md:space-y-12 bg-gray-800 w-full md:w-20 md:min-h-screen">
+      <div
+        className={`duration-700 transition-all ease-in-out md:items-start py-8 flex flex-row md:flex-col items-start space-x-4 md:space-y-12 bg-gray-800 w-full ${
+          isMinimized ? "md:w-12" : "md:w-36"
+        } md:min-h-screen`}
+      >
+        <div className="items-center flex space-x-2">
           <NavItem link="#">
-            <ChevronRightIcon
+            <ChevronLeftIcon
               className={`ml-5 h-8 w-8 text-white ${hoverClass}`}
               onClick={toggleMinimize}
             />
           </NavItem>
+          <h1 className="text-white">Minimize</h1>
+        </div>
+        <div className="items-center flex space-x-2">
           <NavItem link="dashboard">
             <Squares2X2Icon className={`h-8 w-8 text-white ${hoverClass}`} />
           </NavItem>
+          <h1 className="transition ease-in-out delay-150 text-white">
+            Dashboard
+          </h1>
+        </div>
+        <div className="items-center flex space-x-2">
           <NavItem link="create">
             <PlusCircleIcon className={`h-8 w-8 text-white ${hoverClass}`} />
           </NavItem>
+          <h1 className="text-white">Create</h1>
+        </div>
+        <div className="items-center flex space-x-2">
           <NavItem link="calendar">
             <CalendarIcon className={`h-8 w-8 text-white ${hoverClass}`} />
           </NavItem>
+          <h1 className="text-white">Calendar</h1>
+        </div>
+        <div className="items-center flex space-x-2">
           <NavItem link="list/tasks">
             <ClipboardDocumentCheckIcon
               className={`h-8 w-8 text-white ${hoverClass}`}
             />
           </NavItem>
+          <h1 className="text-white">Lists</h1>
         </div>
-      ) : (
-        <div className="md:items-start py-8 flex flex-row md:flex-col items-start space-x-4 md:space-y-12 bg-gray-800 w-full md:w-36 md:min-h-screen">
-          <div className="items-center flex space-x-2">
-            <NavItem link="#">
-              <ChevronLeftIcon
-                className={`ml-5 h-8 w-8 text-white ${hoverClass}`}
-                onClick={toggleMinimize}
-              />
-            </NavItem>
-            <h1 className="text-white">Minimize</h1>
-          </div>
-          <div className="items-center flex space-x-2">
-            <NavItem link="dashboard">
-              <Squares2X2Icon className={`h-8 w-8 text-white ${hoverClass}`} />
-            </NavItem>
-            <h1 className="text-white">Dashboard</h1>
-          </div>
-          <div className="items-center flex space-x-2">
-            <NavItem link="create">
-              <PlusCircleIcon className={`h-8 w-8 text-white ${hoverClass}`} />
-            </NavItem>
-            <h1 className="text-white">Create</h1>
-          </div>
-          <div className="items-center flex space-x-2">
-            <NavItem link="calendar">
-              <CalendarIcon className={`h-8 w-8 text-white ${hoverClass}`} />
-            </NavItem>
-            <h1 className="text-white">Calendar</h1>
-          </div>
-          <div className="items-center flex space-x-2">
-            <NavItem link="list/tasks">
-              <ClipboardDocumentCheckIcon
-                className={`h-8 w-8 text-white ${hoverClass}`}
-              />
-            </NavItem>
-            <h1 className="text-white">My Lists</h1>
-          </div>
-        </div>
-      )}
+      </div>
     </>
   );
 };
