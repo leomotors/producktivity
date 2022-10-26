149ea0a537dc31f433460b9aa969d33693cc4d7b
Idhibhat Pankam <marginpankam@gmail.com>
2022-10-26 08:31:00 +0700

    fixed events page has 2 layout elements

--- apps/client/src/pages/list/events.tsx
+++ apps/client/src/pages/list/events.tsx
@@ -15,11 +15,9 @@ import DefaultLayout from "$core/layouts/default";
 
 const Tasks: MyPage = () => {
   const queryEvents = useEventsQuery();
-  const { data } = await requestLogin({
-    variables: {
-      username,
-    },
-  });
+  const { data } = queryEvents;
+  console.log(data);
+
   interface IEvents {
     id: number;
     name: string;
@@ -90,66 +88,61 @@ const Tasks: MyPage = () => {
   const hoverClass =
     "transition ease-in-out delay-50 duration-150 hover:scale-110 hover:cursor-pointer";
   return (
-    <DefaultLayout>
-      <div className="h-full flex flex-col ml-8 rounded-lg bg-gray-800 w-11/12 overflow-auto">
-        <Tabs active="events"></Tabs>
-        <div className="overflow-auto p-4 flex space-y-4 h-full w-full bg-white">
-          <div className="p-4 flex flex-col space-y-4 h-full w-3/5 bg-white">
-            {events.map(
-              (event, index) =>
-                event.date > yesterday && (
-                  <div
+    <div className="h-full flex flex-col ml-8 rounded-lg bg-gray-800 w-11/12 overflow-auto">
+      <Tabs active="events"></Tabs>
+      <div className="overflow-auto p-4 flex space-y-4 h-full w-full bg-white">
+        <div className="p-4 flex flex-col space-y-4 h-full w-3/5 bg-white">
+          {events.map(
+            (event, index) =>
+              event.date > yesterday && (
+                <div key={index} className="flex items-center w-full space-x-8">
+                  <Event
                     key={index}
-                    className="flex items-center w-full space-x-8"
+                    date={event.date}
+                    name={event.name}
+                    topic={event.topic}
+                  ></Event>
+                  <div
+                    className={`font-bold rounded-lg px-4 py-2 h-fit flex justify-center items-center ${
+                      input.id === event.id ? "bg-lime-300" : "bg-amber-300"
+                    } ${hoverClass}`}
+                    onClick={() => selectEvent(event.id)}
                   >
-                    <Event
-                      key={index}
-                      date={event.date}
-                      name={event.name}
-                      topic={event.topic}
-                    ></Event>
-                    <div
-                      className={`font-bold rounded-lg px-4 py-2 h-fit flex justify-center items-center ${
-                        input.id === event.id ? "bg-lime-300" : "bg-amber-300"
-                      } ${hoverClass}`}
-                      onClick={() => selectEvent(event.id)}
-                    >
-                      {input.id === event.id ? "Editing" : "Edit"}
-                    </div>
-                    <div
-                      className={`font-bold rounded-lg px-4 py-2 h-fit flex justify-center items-center bg-red-600 text-white ${hoverClass}`}
-                      onClick={() => deleteEvent(event.id)}
-                    >
-                      Delete
-                    </div>
+                    {input.id === event.id ? "Editing" : "Edit"}
                   </div>
-                )
-            )}
-          </div>
-          <div className="ml-12 p-4 flex flex-col items-start space-y-4 h-full w-2/5 bg-white">
-            <FormInput
-              handleChange={(e) => updateEvent("name", e.target.value)}
-              name="name"
-              value={input.name}
-            ></FormInput>
-            <TagInput
-              handleChange={(value) => updateEvent("topic", value)}
-              name="topic"
-              value={input.topic}
-            ></TagInput>
-            <DateInput
-              handleChange={(value) => updateEvent("date", value)}
-              name="Date"
-              value={input.date}
-            ></DateInput>
-            <ConfirmButton
-              handleSave={() => saveEvent(input.id)}
-              text={input.id === -1 ? "Add new" : "Save"}
-            ></ConfirmButton>
-          </div>
+                  <div
+                    className={`font-bold rounded-lg px-4 py-2 h-fit flex justify-center items-center bg-red-600 text-white ${hoverClass}`}
+                    onClick={() => deleteEvent(event.id)}
+                  >
+                    Delete
+                  </div>
+                </div>
+              )
+          )}
+        </div>
+        <div className="ml-12 p-4 flex flex-col items-start space-y-4 h-full w-2/5 bg-white">
+          <FormInput
+            handleChange={(e) => updateEvent("name", e.target.value)}
+            name="name"
+            value={input.name}
+          ></FormInput>
+          <TagInput
+            handleChange={(value) => updateEvent("topic", value)}
+            name="topic"
+            value={input.topic}
+          ></TagInput>
+          <DateInput
+            handleChange={(value) => updateEvent("date", value)}
+            name="Date"
+            value={input.date}
+          ></DateInput>
+          <ConfirmButton
+            handleSave={() => saveEvent(input.id)}
+            text={input.id === -1 ? "Add new" : "Save"}
+          ></ConfirmButton>
         </div>
       </div>
-    </DefaultLayout>
+    </div>
   );
 };
 Tasks.Layout = DefaultLayout;
